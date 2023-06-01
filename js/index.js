8806591ae923ec91ccb8665c3ead61f67fbaa5c8
0xh7ml <darkhtml2275@gmail.com>
2023-06-01 23:26:25 +0600

    fixed prob_3 solution

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-let n = 5;
-let myString = "";
-// external loop
-for(let i=1; i<n+1; i++){
-    for(let j=1; j<i+1; j++){
-        myString += j;
-    }
-    myString += "\n";
-}
-console.log(myString);
+let arr = [10,12,14,2,10,5,2]
+arr.forEach(e => {
+    result = 
+});
